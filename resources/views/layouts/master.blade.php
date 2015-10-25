f5147689505527451b389306270a9862d14fff03
Jason Yu <jasonyu@fas.harvard.edu>
2015-10-25 19:15:00 -0400

    Updated master layout view to disable zooming on mobile devices.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>
-        {{-- 
+        {{--
         Yield the title if it exists, otherwise default to
         'Developer's Best Friend'
         --}}
@@ -10,7 +10,7 @@
     </title>
 
     <meta charset='utf-8'/>
-    <meta name='viewport' content='width=device-width, initial-scale=1'/>
+    <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1'/>
 
     <!-- <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' rel='stylesheet'/> -->
     <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' rel='stylesheet'/>
