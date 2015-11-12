b0e553fc0eff332d564bca4b7911e50e5c937b4e
Jason Yu <jasonyu@fas.harvard.edu>
2015-11-12 01:55:16 -0500

    Updated spacing for closures.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,13 +16,13 @@ class AppServiceProvider extends ServiceProvider
     {
         // register custom validation rule for a function attribute
         Validator::extend('function',
-            function($attribute, $value, $parameters, $validator) {
+            function ($attribute, $value, $parameters, $validator) {
                 return function_exists($value);
             });
 
         // register custom validation error message for a function attribute
         Validator::replacer('function',
-            function($message, $attribute, $rule, $parameters) {
+            function ($message, $attribute, $rule, $parameters) {
                 // return the error message with $attribute stripped of special
                 // characters
                 return "The " . preg_replace("/[^A-Za-z0-9]/", " ",
