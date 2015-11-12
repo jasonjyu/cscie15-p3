460a7ffd82522ac022b6b4a0792bb3eb11f31bc0
Jason Yu <jasonyu@fas.harvard.edu>
2015-11-12 02:00:00 -0500

    Updated spacing for closures.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -62,5 +62,5 @@ Use it to add specific things that *this* View needs at the end of the body,
 such as a page specific JavaScript files.
 --}}
 @section('body')
-    {{-- <script src="/js/home.js"></script> --}}
+    {{-- <script src='/js/home.js'></script> --}}
 @stop
